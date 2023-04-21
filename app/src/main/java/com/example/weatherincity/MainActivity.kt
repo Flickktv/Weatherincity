7f1a4fdca5aae0711e591007a949d3e5f39e9a49
Flickktv <tkachev.03@yandex.ru>
2023-04-21 08:19:47 +0500

    1

---
+++ app/src/main/java/com/example/weatherincity/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.weatherincity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val textViewHello = findViewById<TextView>(R.id.tvHello)
+        textViewHello.text = "NO"
+    }
+
+}
